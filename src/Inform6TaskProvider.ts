7bc8a592c3b9414b4d092984c1dc158d0bcbf72e
Natrium729 <natrium729@gmail.com>
2020-07-15 22:13:43 -0400

    Reworked how the task provider works
    Now that I have a better understanding of how a taks providers works, there have been a lot a refactoring.
    
    The Inform 6 task definition now have more properties: source (the only required one), output and iclCommands
    
    The provider doesn't provide tasks at all (since they are none we can auto-detect) and the resolve method now returns a new task according to the given definition. There is a new function to create a new task based on a definition.

--- src/Inform6TaskProvider.ts
+++ src/Inform6TaskProvider.ts
@@ -1,61 +1,111 @@
 /**
- * The Inform 6 Task provider, allowing to compile an Inform 6 source.
- * Based on
- * https://github.com/microsoft/vscode-extension-samples/tree/master/task-provider-sample
+ * The Inform 6 Task provider and other task-related functions.
  */
+
+import path = require("path")
 
 import * as vscode from "vscode"
 
-interface Inform6TaskDefinition extends vscode.TaskDefinition {
-	type: string
+
+export interface Inform6TaskDefinition extends vscode.TaskDefinition {
+	/**
+	 * The type of an Inform 6 task is always `inform6`.
+	 */
+	type: "inform6",
+	/**
+	 * The Inform file to compile.
+	 */
+	source: string,
+	/**
+	 * The destination of the compiled story file.
+	 */
+	output?: string,
+	/**
+	 * The additional ICL commands to pass to the compiler.
+	 */
+	iclCommands?: string[]
 }
 
+
 export class Inform6TaskProvider implements vscode.TaskProvider {
-	static Inform6TaskType = "inform6"
-	private tasks: vscode.Task[] | undefined
+	static readonly Inform6TaskType = "inform6"
 
 	async provideTasks(): Promise<vscode.Task[]> {
-		return this.getTasks()
+		// No tasks are auto-detected for the moment.
+		// TODO: Maybe scan all Inform files in the workspace and create a task for each
+		// one of them ?
+		return []
 	}
 
-	/* This one is simple because the task has no properties, so there's only one to get.
-	Has to change change if other Inform 6 tasks are created. */
 	resolveTask(_task: vscode.Task): vscode.Task | undefined {
-		const definition: Inform6TaskDefinition = <any>_task.definition
-		return this.getTask(definition)
-	}
-
-	private getTasks(): vscode.Task[] {
-		if (this.tasks !== undefined) {
-			return this.tasks
-		}
-		this.tasks = [this.getTask()]
-		return this.tasks
-	}
-
-	private getTask(definition?: Inform6TaskDefinition) : vscode.Task {
-		if (definition === undefined) {
-			definition = {
-				type: Inform6TaskProvider.Inform6TaskType,
-			}
+		// Check if the mandatory source is present.
+		const source = _task.definition.source
+		if (source) {
+			return getInform6Task({
+				type: "inform6",
+				source: source,
+				output: _task.definition.output || undefined,
+				iclCommands: _task.definition.iclCommands || undefined
+			})
 		}
 
-		// Get the compiler and its args from the settings.
-		const compiler = vscode.workspace.getConfiguration("inform6").get<string>("inform6Path") || "inform6"
-		const compilerArgs = vscode.workspace.getConfiguration("inform6").get<string[]>("compilerCommands") || []
-
-		// Add the mandatory args.
-		compilerArgs.push("-c") // No code excerpt in error messages.
-		compilerArgs.push("-E1") // Microsoft error style (for the problem matcher).
-		compilerArgs.push("${file}") // The source to compile.
-
-		return new vscode.Task(
-			definition,
-			vscode.TaskScope.Workspace,
-			"compile",
-			"inform6",
-			new vscode.ProcessExecution(compiler, compilerArgs),
-			"$inform6"
-		)
+		return undefined
 	}
+}
+
+/**
+ * Create an Inform 6 task from an Inform 6 task definition.
+ *
+ * The task will use the compiler and the compiler commands from the settings The latter
+ * will be overriden by the ICL commands provided in the definition.
+ *
+ * @param definition - The Inform 6 task definition to use.
+ */
+export function getInform6Task(definition: Inform6TaskDefinition): vscode.Task {
+	// Get the compiler from the settings.
+	// TODO: add compiler as a task definition field so that a user provided task can
+	// specify a compiler?
+	const compiler = vscode.workspace.getConfiguration("inform6").get<string>("inform6Path", "inform6")
+
+	// Will contains the arguments passed to the compiler.
+	let args: string[]
+
+	// Get the global ICL commands from the settings.
+	const settingsArgs = vscode.workspace.getConfiguration("inform6").get<string[]>("compilerCommands")
+
+
+	// We spread the args instead of using the array from the settings above directly,
+	// because VS Code will always return the same object from the config instead of a
+	// new copy, and it will mutate in place, causing errors (we need a brand new array
+	// for each new task).
+	if (settingsArgs) {
+		args = [...settingsArgs]
+	} else {
+		args = []
+	}
+
+	// Add the ICL commands from the task definition.
+	if (definition.iclCommands) {
+		args.push(...definition.iclCommands)
+	}
+
+	// Add the mandatory args.
+	args.push("-c") // No code excerpt in error messages.
+	args.push("-E1") // Microsoft error style (for the problem matcher).
+	args.push(definition.source) // The source to compile.
+	if (definition.output) {
+		args.push(definition.output) // The destination of the compiled file if present.
+	}
+
+	return new vscode.Task(
+		definition,
+		vscode.TaskScope.Workspace,
+		`compile ${path.basename(definition.source)}`,
+		"inform6",
+		new vscode.ProcessExecution(compiler, args, {
+			// So that the story file is next to the source.
+			cwd: path.dirname(definition.source)
+		}),
+		"$inform6"
+	)
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,21 @@
 import * as vscode from "vscode"
 
-import { Inform6TaskProvider } from "./Inform6TaskProvider"
+import { Inform6TaskProvider, getInform6Task } from "./Inform6TaskProvider"
+
 
 /**
  * The current Inform 6 task provider.
  */
 let inform6TaskProvider: vscode.Disposable | undefined
 
+
 export function activate(context: vscode.ExtensionContext): void {
-	registerInform6TaskProvider()
+	inform6TaskProvider = vscode.tasks.registerTaskProvider(
+		Inform6TaskProvider.Inform6TaskType,
+		new Inform6TaskProvider()
+	)
 
-	// Register a new Inform 6 task provider when settings affecting it change.
-	context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(e => {
-		if (e.affectsConfiguration("inform6.inform6Path") || e.affectsConfiguration("inform6.compilerCommands")) {
-			registerInform6TaskProvider()
-		}
-	}))
-
-	const command_compile = vscode.commands.registerCommand("inform6.compile", (uri: vscode.Uri | undefined) => {
+	const compileCommand = vscode.commands.registerCommand("inform6.compile", (uri: vscode.Uri | undefined) => {
 		// When using the command palette, no uri is provided.
 		// We use the currently active editor in that case.
 		if (!uri) {
@@ -29,37 +27,21 @@ export function activate(context: vscode.ExtensionContext): void {
 		}
 		compile(uri)
 	})
-	context.subscriptions.push(command_compile)
+	context.subscriptions.push(compileCommand)
 }
 
-/**
- * Dispose of the previous Inform 6 task provider if one exists and register a new one.
- */
-function registerInform6TaskProvider() {
-	if (inform6TaskProvider) {
-		inform6TaskProvider.dispose()
-	}
-	inform6TaskProvider = vscode.tasks.registerTaskProvider(
-		Inform6TaskProvider.Inform6TaskType,
-		new Inform6TaskProvider()
-	)
-}
 
 /**
  * Execute the Inform 6 task to compile the given file.
  * @param uri The uri of the file to compile.
  */
-async function compile(uri: vscode.Uri) {
-	/* We open the file being compile so that the `${file}` variable, which the task uses, is defined. */
-	vscode.commands.executeCommand("vscode.open", uri)
-
-	/* We directly take the first task returned by the provider, because there is only one task created by the provider.
-	This will have to be changed should the extension create more Inform 6 tasks. */
-	const tasks = await vscode.tasks.fetchTasks({
-		type: Inform6TaskProvider.Inform6TaskType
-	})
-	vscode.tasks.executeTask(tasks[0])
+function compile(uri: vscode.Uri) {
+	vscode.tasks.executeTask(getInform6Task({
+		type: Inform6TaskProvider.Inform6TaskType,
+		source: uri.fsPath
+	}))
 }
+
 
 export function deactivate(): void {
 	if (inform6TaskProvider) {
